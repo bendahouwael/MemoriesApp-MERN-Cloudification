2a6cfe1d705dda189da729a1d6efac2181ab82d4
wael ben dahou <wael.bendahou@supcom.tn>
2021-10-21 10:33:55 +0200

    first commit

---
+++ client/build/precache-manifest.861b61bb236b7da7fe39f20787e65100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3745e2747a7afe62a064374aef6310ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca59b5d0ae7d7d03e8f4",
+    "url": "/static/css/main.42acf6b3.chunk.css"
+  },
+  {
+    "revision": "4dbafc9680a83ea712b9",
+    "url": "/static/js/2.4e7d4614.chunk.js"
+  },
+  {
+    "revision": "6f001b22a9cb1fcb0d750f6c56d216df",
+    "url": "/static/js/2.4e7d4614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca59b5d0ae7d7d03e8f4",
+    "url": "/static/js/main.990067ed.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
